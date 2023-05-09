e5ed50dda9ddb87886efa440a0401822037b6077
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-10 08:29:38 +0900

    ContinueSample 클래스 작성

--- src/main/java/kr/ac/sahmyook/home/func/ContinueSample.java
+++ src/main/java/kr/ac/sahmyook/home/func/ContinueSample.java
@@ -2,6 +2,27 @@ package kr.ac.sahmyook.home.func;
 
 public class ContinueSample {
 
-    public void sumJumpThree(){}
-    public void rowColJump(){}
+    public void sumJumpThree(){
+        int sum = 0;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i % 3 == 0) {
+                continue; 
+            }
+            sum += i;
+        }
+        System.out.println("sum = " + sum);
+    }
+    public void rowColJump(){
+        int[][] arr = new int[3][5];
+        for(int i = 0; i < 3; i++ ){
+            for (int j = 0; j < 5; j ++){
+                if(j == 1 && j >=2 && j <= 4 ){
+                    continue;
+                }
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
