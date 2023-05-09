d93751656f87df002a4253d745b36f7609f21a42
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-10 03:52:05 +0900

    PrimitiveTypeSample 클래스 작성

--- src/main/java/kr/ac/sahmyook/home/func/PrimitiveTypeSample.java
+++ src/main/java/kr/ac/sahmyook/home/func/PrimitiveTypeSample.java
@@ -2,5 +2,17 @@ package kr.ac.sahmyook.home.func;
 
 public class PrimitiveTypeSample {
 
-    public void typeSize(){}
+    public void typeSize(){
+        System.out.println("=========== 기본 자료형 크기 ===========");
+        System.out.println("Byte.SIZE = " + Byte.SIZE);
+        System.out.println("Short.SIZE = " + Short.SIZE);
+        System.out.println("Integer.SIZE = " + Integer.SIZE);
+        System.out.println("Long.SIZE = " + Long.SIZE);
+        System.out.println("Float.SIZE = " + Float.SIZE);
+        System.out.println("Float.SIZE = " + Float.SIZE);
+        System.out.println("Double.SIZE = " + Double.SIZE);
+        System.out.println("Character.SIZE = " + Character.SIZE);
+        System.out.println("=======================================");
+        System.out.println();
+    }
 }
