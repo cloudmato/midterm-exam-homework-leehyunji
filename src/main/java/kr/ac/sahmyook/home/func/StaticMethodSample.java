d396e5748dbd10487c22e1719fe15a169f9c7a33
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-10 08:39:12 +0900

    StaticMethodSample 클래스 작성

--- src/main/java/kr/ac/sahmyook/home/func/StaticMethodSample.java
+++ src/main/java/kr/ac/sahmyook/home/func/StaticMethodSample.java
@@ -2,7 +2,16 @@ package kr.ac.sahmyook.home.func;
 
 public class StaticMethodSample {
 
-    public void testMathRandom(){}
-    public void testMathAbs(){}
-    public void testMathMax(){}
+    public void testMathRandom(){
+        int random = (int)((Math.random()*45)+1);
+        System.out.println("random = " + random);
+    }
+    
+    public void testMathAbs(){
+        double num = Math.abs(-12.77);
+        System.out.println("num = " + num);
+    }
+    public void testMathMax(){
+        System.out.println("Math.max(120, 54) = " + Math.max(120, 54));
+    }
 }
