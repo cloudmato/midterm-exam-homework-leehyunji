60fb9fc74a045ae70f33b752bbb73a380eb2f9f2
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-10 08:30:37 +0900

    BreakSample 클래스 작성

--- src/main/java/kr/ac/sahmyook/home/func/BreakSample.java
+++ src/main/java/kr/ac/sahmyook/home/func/BreakSample.java
@@ -2,6 +2,24 @@ package kr.ac.sahmyook.home.func;
 
 public class BreakSample {
 
-    public void sumBreak(){}
-    public void guguDanBreak(){}
+    public void sumBreak(){
+        int sum = 0;
+        int i = 1;
+        while(true) {
+            if (i != 101){
+                sum += i;
+                i++;}
+            else break;
+        }
+        System.out.println("sum = " + sum);
+    }
+    public void guguDanBreak(){
+        for (int i = 0; i < 9; i++){
+            for (int j = 0; j < 9; j++) {
+                if(j == 5) break;
+                System.out.println(i + " x " + j + " = " + i*j);
+            }
+            System.out.println();
+        }
+    }
 }
