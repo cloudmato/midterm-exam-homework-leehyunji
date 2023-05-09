7ccc104a455c6e9404c08d023a100c42e8e74e43
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-10 08:38:29 +0900

    NonStaticMethodSample 클래스 작성

--- src/main/java/kr/ac/sahmyook/home/func/NonStaticMethodSample.java
+++ src/main/java/kr/ac/sahmyook/home/func/NonStaticMethodSample.java
@@ -1,7 +1,40 @@
 package kr.ac.sahmyook.home.func;
 
+import java.util.Scanner;
+
 public class NonStaticMethodSample {
 
-    public void testScanner(){}
-    public void testRandom(){}
+    public void testScanner(){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Character : ");
+        char c = sc.nextLine().charAt(0);
+        System.out.println("Integer : ");
+        int i = sc.nextInt();
+        System.out.println("short : ");
+        short s = sc.nextShort();
+        System.out.println("long : ");
+        long l = sc.nextLong();
+        System.out.println("float : ");
+        float f = sc.nextFloat();
+        System.out.println("double : ");
+        double d = sc.nextDouble();
+
+        System.out.println("c = " + c);
+        System.out.println("i = " + i);
+        System.out.println("s = " + s);
+        System.out.println("l = " + l);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
+
+    }
+    public void testRandom(){
+
+        int randomInt = (int) (Math.random() * 10);
+        System.out.println("Random Integer: " + randomInt);
+
+
+        double randomDouble = Math.random();
+        System.out.println("Random Double: " + randomDouble);
+    }
 }
