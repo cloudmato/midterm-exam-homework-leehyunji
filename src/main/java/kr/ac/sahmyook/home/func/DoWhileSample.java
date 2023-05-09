29ec48dd2aff9bdb121d18df477ad7494cf9a3a1
DESKTOP-5NMV1JN\Hyeonji Lee <cloudmato00@gmail.com>
2023-05-10 08:34:27 +0900

    DoWhileSample 클래스 작성(버거킹 문제 제외)

--- src/main/java/kr/ac/sahmyook/home/func/DoWhileSample.java
+++ src/main/java/kr/ac/sahmyook/home/func/DoWhileSample.java
@@ -1,8 +1,51 @@
 package kr.ac.sahmyook.home.func;
 
+import java.util.Scanner;
+
 public class DoWhileSample {
 
-    public void addDashToken(){}
-    public void burgerKingMenu(){}
-    public void isStringAlphabet(){}
-}
+    public void addDashToken(){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("문자열을 입력하세요. : ");
+        String input = sc.nextLine();
+        sc.close();
+
+        StringBuilder output = new StringBuilder();
+        for (int i = 0; i < input.length(); i++) {
+            output.append(input.charAt(i));
+            if (i != input.length() - 1) {
+                output.append('-');
+            }
+        }
+        System.out.println("결과: " + output.toString());
+    }
+    public void burgerKingMenu(){
+
+    }
+    public void isStringAlphabet(){
+        Scanner sc = new Scanner(System.in);
+        String input;
+        boolean isAllAlpha = true;
+
+        do {
+            System.out.print("문자열을 입력하세요: ");
+            input = sc.nextLine();
+
+            for (int i = 0; i < input.length(); i++) {
+                if (!(input.charAt(i) >= 'a' && input.charAt(i) <= 'z') &&
+                        !(input.charAt(i) >= 'A' && input.charAt(i) <= 'Z')) {
+                    isAllAlpha = false;
+                    break;
+                }
+            }
+
+            if (!isAllAlpha) {
+                System.out.println("모든 글자가 영문자가 아닙니다.");
+            }
+
+        } while (!isAllAlpha);
+
+        System.out.println("모든 글자가 영문자입니다.");
+        sc.close();
+        }
+    }
